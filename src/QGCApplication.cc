0fce9b5de5f5fd682550a61a3f36014045437dc5
adrien <adrien.papin@drone-geofencing.com>
2024-11-21 09:21:57 +0100

    back to real

--- src/QGCApplication.cc
+++ src/QGCApplication.cc
@@ -1346,7 +1346,7 @@ void QGCApplication::startStream(){
 
     // Start the bus thread
     std::thread threadBus([this]() -> void {
-        const gchar* pipelineDesc = "rtspsrc location=rtsp://localhost:8554/city-traffic is-live=true latency=0 protocols=tcp ! rtpbin ! decodebin ! videoconvert ! x264enc ! flvmux streamable=true ! rtmpsink location=rtmp://ome.stationdrone.net/app/city-traffic";
+        const gchar* pipelineDesc = "rtspsrc location=rtsp://192.168.144.25:8554/main.264 is-live=true latency=0 protocols=tcp ! rtpbin ! decodebin ! videoconvert ! x264enc ! flvmux streamable=true ! rtmpsink location=rtmp://ome.stationdrone.net/app/1600FTR2STD24289930B";
         GError *err = nullptr; // RECUP COMMANDE DANS WSL
         this->data.pipeline = gst_parse_launch(pipelineDesc, &err);
 
