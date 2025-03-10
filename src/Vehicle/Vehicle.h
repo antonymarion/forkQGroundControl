e81a4e991582f834cf57d6ffb203dfb5626694d5
adrien <adrien.papin@drone-geofencing.com>
2025-03-10 12:13:57 +0100

    fix

--- src/Vehicle/Vehicle.h
+++ src/Vehicle/Vehicle.h
@@ -1172,7 +1172,7 @@ private:
 
     QMap<QString, QStringList> aircraftUasSnList = {
         {"4F:4E:49:44:4C:41:54:49", {"UAS-FR-651384", "1H82582569285", "Gazebo"}},
-        {"00:00:00:00:00:00:00:00", {"UAS-FR-652384", "2H82582569285", "ArduCopter"}}
+        {"00:00:00:00:00:00:00:00", {"UAS-FR-652384", "2H82582569285", "ArduCopter"}},
         {"20:97:27:49:3B:2F", {"UAS-FR-486654", "20:97:27:49:3B:2F", "SL-450-NG"}} // redefine serial number
     };
 
