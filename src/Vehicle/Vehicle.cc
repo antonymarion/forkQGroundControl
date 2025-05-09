e0cab600fc14038b867c36c01cc392ea5344b157
adrien <adrien.papin@drone-geofencing.com>
2025-05-09 16:14:13 +0200

    check firmware

--- src/Vehicle/Vehicle.cc
+++ src/Vehicle/Vehicle.cc
@@ -833,7 +833,7 @@ void Vehicle::_setNewVehicleData()
 {
     QMap<QString, QStringList> uasSnMap = qgcApp()->getAircraftInfo();
     QStringList uasSn;
-    if(false){ // change if simulated or not
+    if(true){ // change if simulated or not
         qCWarning(VehicleLog) << "=======================================";
         qCWarning(VehicleLog) << "This uav id : " << _id;
         qCWarning(VehicleLog) << "=======================================";
