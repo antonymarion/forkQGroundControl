4e4903dae470d54f486d3dd2d789328ca003f0ee
adrien <adrien.papin@drone-geofencing.com>
2025-06-06 13:16:54 +0200

    fix

--- src/Vehicle/Vehicle.cc
+++ src/Vehicle/Vehicle.cc
@@ -3181,11 +3181,6 @@ void Vehicle::guidedModeRTL(bool smartRTL)
     _firmwarePlugin->guidedModeRTL(this, smartRTL);
 }
 
-void Vehicle::offboardMode()
-{
-    _firmwarePlugin->offboardMode(this);
-}
-
 void Vehicle::guidedModeLand()
 {
     if (!guidedModeSupported()) {
