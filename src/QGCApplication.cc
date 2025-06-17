2cec4d7e67c1e920698bfe56522ff84c5e4ee135
adrien <adrien.papin@drone-geofencing.com>
2025-06-17 13:08:43 +0200

    fix

--- src/QGCApplication.cc
+++ src/QGCApplication.cc
@@ -2250,7 +2250,7 @@ void QGCApplication::pauseVehicle(Vehicle* requestVehicle, const QMqttMessage& m
     double w_yaw = (qobject_cast<Fact*>(requestVehicle->heading())->rawValueString()).toDouble();
     goToWaypoint(requestVehicle, w_speed, w_yaw, w_lat, w_lon, w_alt, msg, message, state_value);
     
-    QObject::connect(requestVehicle, &Vehicle::repositionResult, this, [this, msg, message, requestVehicle, state_value](bool success) {
+    QObject::connect(requestVehicle, &Vehicle::repositionResult, this, [this, msg, message, requestVehicle, &state_value](bool success) {
         double w_lat = requestVehicle->coordinate().latitude();
         double w_lon = requestVehicle->coordinate().longitude();
         double w_alt = requestVehicle->coordinate().altitude();
