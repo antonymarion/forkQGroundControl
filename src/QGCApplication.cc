9bc4741e2fa0234efe0680aa5d2bcadea749dd76
adrien <adrien.papin@drone-geofencing.com>
2025-06-17 12:31:22 +0200

    change pause

--- src/QGCApplication.cc
+++ src/QGCApplication.cc
@@ -2250,13 +2250,14 @@ void QGCApplication::pauseVehicle(Vehicle* requestVehicle, const QMqttMessage& m
     double w_yaw = (qobject_cast<Fact*>(requestVehicle->heading())->rawValueString()).toDouble();
     goToWaypoint(requestVehicle, w_speed, w_yaw, w_lat, w_lon, w_alt, msg, message, state_value);
     
-    QThread::msleep(1000);
-    
-    w_lat = requestVehicle->coordinate().latitude();
-    w_lon = requestVehicle->coordinate().longitude();
-    w_alt = requestVehicle->coordinate().altitude();
-    w_yaw = (qobject_cast<Fact*>(requestVehicle->heading())->rawValueString()).toDouble();
-    goToWaypoint(requestVehicle, w_speed, w_yaw, w_lat, w_lon, w_alt, msg, message, state_value);
+    QObject::connect(requestVehicle, &Vehicle::repositionResult, this, [this, msg, message, requestVehicle](bool success) {
+        double w_lat = requestVehicle->coordinate().latitude();
+        double w_lon = requestVehicle->coordinate().longitude();
+        double w_alt = requestVehicle->coordinate().altitude();
+        double w_yaw = (qobject_cast<Fact*>(requestVehicle->heading())->rawValueString()).toDouble();
+        goToWaypoint(requestVehicle, w_speed, w_yaw, w_lat, w_lon, w_alt, msg, message, state_value);
+        QObject::disconnect(requestVehicle, &Vehicle::repositionResult, this, nullptr);
+    });
 
 }
 
