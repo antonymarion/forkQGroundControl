c30ab3ba52a1f353c2446760d6ba37628977d1c8
adrien <adrien.papin@drone-geofencing.com>
2025-06-19 10:50:49 +0200

    fix mask

--- src/Vehicle/Vehicle.cc
+++ src/Vehicle/Vehicle.cc
@@ -1068,7 +1068,13 @@ void Vehicle::sendSetPositionTargetLocalNed(double vx, double vy, double vz, dou
     cmd.target_system = id(); // Target system ID (the drone)
     cmd.target_component = _defaultComponentId; // Target component ID (autopilot)
     cmd.coordinate_frame = MAV_FRAME_BODY_NED; // Reference frame
-    cmd.type_mask = 0b0000111111000000; // Bitmask to indicate which dimensions should be ignored by the vehicle
+    cmd.type_mask = POSITION_TARGET_TYPEMASK_X_IGNORE |
+                    POSITION_TARGET_TYPEMASK_Y_IGNORE |
+                    POSITION_TARGET_TYPEMASK_Z_IGNORE |
+                    POSITION_TARGET_TYPEMASK_AX_IGNORE |
+                    POSITION_TARGET_TYPEMASK_AY_IGNORE |
+                    POSITION_TARGET_TYPEMASK_AZ_IGNORE |
+                    POSITION_TARGET_TYPEMASK_YAW_IGNORE; // Bitmask to indicate which dimensions should be ignored by the vehicle
     // The type_mask is set to ignore position and acceleration, allowing velocity and yaw rate to be set
 
     mavlink_msg_set_position_target_local_ned_encode_chan(
