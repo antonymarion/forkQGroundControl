75a91a8a8c41f074235922e6ade411c2d9478ec2
mafaiz <mohammed.amine.faiz@drone-geofencing.com>
2025-07-18 16:56:13 +0200

    Json problem

--- src/QGCApplication.cc
+++ src/QGCApplication.cc
@@ -2732,7 +2732,7 @@ bool QGCApplication::event(QEvent *e)
     return QApplication::event(e);
 }
 
-void QGCApplication::sendMission(const QString& mission)
+void QGCApplication::sendMission(QString mission)
 {
     QString outputDir = QStandardPaths::writableLocation(QStandardPaths::DocumentsLocation);
 
