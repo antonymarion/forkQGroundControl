f992fc01b47745f2781ec83068193e5312eb2c56
mafaiz <mohammed.amine.faiz@drone-geofencing.com>
2025-07-21 10:00:57 +0200

    Json problem

--- src/QGCApplication.cc
+++ src/QGCApplication.cc
@@ -2757,7 +2757,7 @@ void QGCApplication::sendMission(QString mission)
     out << mission;
     file.close();
 
-    qDebug() << mission;
+    qDebug() << "Mission content:" << mission;
 
     PlanMasterController* planController = new PlanMasterController(nullptr);
     planController->start();
