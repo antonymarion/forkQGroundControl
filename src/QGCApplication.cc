5e966dc4e8f0d8f2a910f03ba6cd731d05d18cfc
mafaiz <mohammed.amine.faiz@drone-geofencing.com>
2025-07-29 11:41:50 +0200

    Add Suivi mission: debug

--- src/QGCApplication.cc
+++ src/QGCApplication.cc
@@ -2887,9 +2887,9 @@ void QGCApplication::sendMissionInstruction(QString           clientId,
     double currentLatitude=0.0, currentLongitude=0.0, currentAltitude=0.0;
     double lat_tolerance=0.00002, long_tolerance=0.00002, alt_tolerance=1;
 
-    _gpsRtkFactGroup->currentLatitude()->setRawValue(currentLatitude);
-    _gpsRtkFactGroup->currentLongitude()->setRawValue(currentLongitude);
-    _gpsRtkFactGroup->currentAltitude()->setRawValue(currentAltitude);
+    currentLatitude = _gpsRtkFactGroup->currentLatitude();
+    currentLongitude = _gpsRtkFactGroup->currentLongitude();
+    currentAltitude = _gpsRtkFactGroup->currentAltitude();
 
     qDebug() << "currentLatitude" << currentLatitude;
     qDebug() << "currentLongitude" << currentLongitude;
