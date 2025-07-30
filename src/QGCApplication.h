0f3d9de2f9936ddf71bd6f07211c0301787da2af
mafaiz <mohammed.amine.faiz@drone-geofencing.com>
2025-07-30 10:54:56 +0200

    Add new mqtt client for publishing mission commands

--- src/QGCApplication.h
+++ src/QGCApplication.h
@@ -340,6 +340,7 @@ private:
     // QString               uavSn               = "1600FTR2STD24289930B";  // Aircraft serial number
     bool                    isStreaming         = false;                   // is currently streaming on rtmp URL
     QMqttClient*            m_client            = nullptr;                 // mqtt client
+    QMqttClient*            m_client_mission    = nullptr;                 // mqtt client for mission
     bool                    clientState         = false;
     bool                    _recording;
     bool                    canControl          = true;                    // false if remote pilote override commands
