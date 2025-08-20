3f572399639b50554def1367f4c82dc1a7c6998b
adrien <adrien.papin@drone-geofencing.com>
2025-08-20 15:23:47 +0200

    print online

--- src/Vehicle/Vehicle.cc
+++ src/Vehicle/Vehicle.cc
@@ -888,7 +888,7 @@ void Vehicle::setNewVehicleData()
     
     QMap<QString, QStringList> uasSnMap = qgcApp()->getAircraftInfo();
     QStringList uasSn;
-    if(true){ // change if simulated or not
+    if(false){ // change if simulated or not
         qCWarning(VehicleLog) << "======================================";
         qCWarning(VehicleLog) << "This uav id : " << _id;
         qCWarning(VehicleLog) << "======================================";
